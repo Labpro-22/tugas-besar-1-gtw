cb5554dcf8877f61566c895519bc094a2219e297
pidipam <vara.azzara@gmail.com>
2026-04-26 20:14:07 +0700

    feat: kelas konstanta

---
+++ src/utils/GameConstants.cpp
@@ -0,0 +1,5 @@
+#include "utils/GameConstants.hpp"
+
+const int GameConstants::DENDA_PENJARA = 500;
+const int GameConstants::MAX_PERCOBAAN_PENJARA = 3;
+const int GameConstants::SALDO_AWAL_DEFAULT = 1500;
