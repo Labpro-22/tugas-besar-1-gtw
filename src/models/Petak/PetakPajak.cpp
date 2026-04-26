433ee0e56b48b79d338d210a9c1d9538e078cfcc
helenakristela <hkristela03@gmail.com>
2026-04-27 02:00:17 +0700

    solve conflict after merge

--- src/models/Petak/PetakPajak.cpp
+++ src/models/Petak/PetakPajak.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "models/Petak/PetakPajak.hpp"
 #include "models/Pemain.hpp"
 #include "models/PlayerActionService.hpp"
@@ -62,41 +61,3 @@ void PetakPajak::onLanded(Pemain& pemain, PlayerActionService& svc) {
     }
 }
 
-=======
-#include "models/Petak/PetakPajak.hpp"
-#include "models/Pemain.hpp"
-#include "models/PlayerActionService.hpp"
-#include "core/BangkrutDanEndGame.hpp"
-#include "views/OutputHandler.hpp"
-#include "views/InputHandler.hpp"
-
-PetakPajak::PetakPajak(int indeks, const std::string& kode, const std::string& nama, ConfigData* konfigurasi)
-    : PetakAksi(indeks, kode, nama), config(konfigurasi) {
-    // Pastikan jenis pajak terdefinisi (sebelumnya uninitialized).
-    // Mapping dari kode konfigurasi aksi.txt: "PPH" dan "PBM".
-    if (kode == "PBM") {
-        jenis = JenisPajak::PAJAK_BARANG_MEWAH;
-    } else {
-        jenis = JenisPajak::PAJAK_PENGHASILAN;
-    }
-}
-
-// Override method: onLanded
-void PetakPajak::onLanded(Pemain& pemain, PlayerActionService& svc) {
-    if (jenis == JenisPajak::PAJAK_BARANG_MEWAH) {
-        svc.transferMoney(&pemain, nullptr, config->getPbmFlat());
-    }
-    else {
-        int pilihan = InputHandler::mintaPilihanPajak(config->getPphFlat(), config->getPphPersen());
-
-        if (pilihan == 1) {
-            svc.transferMoney (&pemain, nullptr, config->getPphFlat());
-        }
-        else {
-            int harusDibayar = 1.0*Likuidasi::totalLikuidasiSeluruhAsetJual(&pemain)*config->getPphPersen() / 100;
-            svc.transferMoney (&pemain, nullptr, harusDibayar);
-        }
-    }
-}
-
->>>>>>> 4c863580e8432dbd781567a56c63fe50b90b3f39
--- src/models/Petak/PetakProperti.cpp
+++ src/models/Petak/PetakProperti.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "models/Pemain.hpp"
 #include "models/Petak/PetakProperti.hpp"
 #include "models/Petak/PetakStasiun.hpp"
@@ -98,78 +97,4 @@ std::string PetakProperti::getPemilikString() const {
         return "BANK";
     }
     return pemilik->getUsername();
-=======
-#include "models/Pemain.hpp"
-#include "models/Petak/PetakProperti.hpp"
-#include "models/PlayerActionService.hpp"
-#include <iostream>
-
-PetakProperti::PetakProperti(int indeks, const std::string& kode, const std::string& nama, const PropertiConfig& config) : 
-    Petak(indeks, kode, nama), config(&config), pemilik(nullptr), status(StatusProperti::BANK) {}
-
-void PetakProperti::onLanded(Pemain& pemain, PlayerActionService& actionService) {
-    // For now, delegate to PlayerActionService until Phase 3 rewrites this.
-    // actionService.handleLandedOnProperti(pemain, *this);
-    std::cout << "Mendarat di properti: " << nama << " (" << kode << ")\n";
-}
-
-PetakProperti::StatusProperti PetakProperti::getStatus() const {
-    return status;
-}
-
-void PetakProperti::setStatus(StatusProperti s) {
-    status = s;
-}
-
-Pemain* PetakProperti::getPemilik() const {
-    return pemilik;
-}
-
-void PetakProperti::setPemilik(Pemain* pemain) {
-    pemilik = pemain;
-    status = StatusProperti::OWNED;
-}
-
-const PropertiConfig* PetakProperti::getConfigProperti() const {
-    return config;
-}
-
-void PetakProperti::gadai() {
-    status = StatusProperti::MORTGAGED;
-}
-
-void PetakProperti::batalGadai() {
-    status = StatusProperti::OWNED;
-}
-
-int PetakProperti::getNilaiGadai() const {
-    return config->getNilaiGadai();
-}
-
-std::string PetakProperti::getJenisString() const {
-    return config->getJenis();
-}
-
-std::string PetakProperti::getBangunanString() const {
-    return "0";
-}
-
-std::string PetakProperti::getStatusString() const {
-    switch (status) {
-    case StatusProperti::BANK:
-        return "BANK";
-    case StatusProperti::OWNED:
-        return "OWNED";
-    case StatusProperti::MORTGAGED:
-        return "MORTGAGED";
-    }
-    return "BANK";
-}
-
-std::string PetakProperti::getPemilikString() const {
-    if (pemilik == nullptr) {
-        return "BANK";
-    }
-    return pemilik->getUsername();
->>>>>>> 4c863580e8432dbd781567a56c63fe50b90b3f39
 }
